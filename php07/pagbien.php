fdb6be86ab8ac78eb4255760df1be3efcc76573e
vandaleep <vandaleep@hotmail.com>
2025-07-16 11:37:26 -0600

    2025-07-16 1137

--- php07/pagbien.php
+++ php07/pagbien.php
@@ -1,7 +1,12 @@
 <?php
-	//Esta pagina solo se carga SI y solo SI
-	//Hay una cookie válida
-	if (!(isset($_COOKIE["galletita"]) && $_COOKIE["galletita"] == "EN_SESION")) {
+	//Esta pagina solo se carga SI y solo SI hay una cookie válida... salvo que venga de POST
+	if (isset($_POST["accion"]) && $_POST["accion"] == "SALIR") {
+		//Destruir cookie
+		setcookie("galletita", "EN_SESION", time() - 1, "/");
+		echo "<script>document.location.href='login.php';</script>";
+		die();
+	}
+	else if (!(isset($_COOKIE["galletita"]) && $_COOKIE["galletita"] == "EN_SESION")) {
 		echo "<script>document.location.href='pagmal.html';</script>";
 		die();
 	}
@@ -14,6 +19,7 @@
 		<form method="POST">
 			<h1>BIENVENIDO</h1>
 			<input type="submit" value="Salir">
+			<input type="hidden" name="accion" value="SALIR">
 		</form>
 	</body>
 </html> 
