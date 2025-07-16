84022ebf20aea5eef38ddfe4684a604ebae629e0
vandaleep <vandaleep@hotmail.com>
2025-07-16 11:14:19 -0600

    2025-07-16 1114

---
+++ php07/login.php
@@ -0,0 +1,37 @@
+<?php
+	//La página viene de un envío POST
+	if (isset($_POST["login"]) && isset($_POST["pass"])) {
+		//TODO: Estos datos deben venir de una BD
+		$loginValido = "admin";
+		$passValido = "1234";
+		
+		//Credenciales válidas
+		if($_POST["login"] == $loginValido
+			&& $_POST["pass"] == $passValido) {
+			echo "<script>document.href='pagbien.php';</script>";
+			die();
+		} //Credenciales inválidas
+		else {
+			"<script>document.href='pagmal.php';</script>";
+		}
+	}
+	//La página se carga por primera vez
+	else {
+		//Verificar si hay cookie válida
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<form method="POST">
+			<label>Login</label>
+			<input name="login" required><br>
+			<label>Password</label>
+			<input type="password" name="pass" required><br>
+			<input type="checkbox" name="recuerdame">Recuerdame
+			<input type="submit" value="Entrar">
+		</form>
+	</body>
+</html>
---
+++ php07/pagbien.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<form method="POST">
+			<h1>BIENVENIDO</h1>
+			<input type="submit" value="Salir">
+		</form>
+	</body>
+</html> 
